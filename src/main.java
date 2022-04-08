95db2928afdbaca882089b26c9cbce4bfa786165
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-04-08 00:28:29 -0400

    Main

--- src/main.java
+++ src/main.java
@@ -1,11 +1,14 @@
 public class main {
     public static void main(String[] args) {
+        //Ejercicio 1
         //Practica1 practica1=new Practica1();
         //practica1.ejecutar();
-
+        
+        //Ejercicio 2
         //Practica2 practica2=new Practica2();
         //practica2.ejecutar();
         
+        //Ejercicio 3
         Practica3 ejercicio3 = new Practica3();
         ejercicio3.ejecutar();
     }
